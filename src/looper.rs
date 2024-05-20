ac4ad083c3271f5ba458bb707ae20bd3d389bca8
rtu <rtu@ableton.com>
2024-05-20 20:02:36 +0200

    WIP add fade to looper, probably get rid of this technique

--- src/looper.rs
+++ src/looper.rs
@@ -1,13 +1,23 @@
-// looper. simple looper that just loops over a certain segment of the delay line
-// the delay line is assumed to not be being ticked, so the data is stationary
+
 use crate::delay_line::DelayLine;
 
+// looper. simple looper that just loops over a certain segment of the delay line
+// the delay line is assumed to not be being ticked, so the data is stationary
+// the subtleties of where exactly the loop should be are up to the client
 pub struct Looper {
     delay_line: DelayLine,
+    
     is_looping: bool,
+
     loop_start: usize,
     loop_end: usize,
+
+    fade_loop_start: usize,
+    fade_loop_end: usize,
+    
     current_read_position: usize,
+    fading_read_position: usize,
+    fade_length_samples: usize,
 }
 
 // wraps an unsigned integer into a given range [min, max]
@@ -40,7 +50,11 @@ impl Looper
             is_looping: false,
             loop_start: 10,
             loop_end: 0,
+            fade_loop_start: 10,
+            fade_loop_end: 0,
             current_read_position: 0,
+            fading_read_position: 0,
+            fade_length_samples: 0,
         }
     }
 
@@ -51,8 +65,20 @@ impl Looper
         assert!(start > end);
         self.loop_start = start;
         self.loop_end = end;
-        // wrap the current read position into the loop
+        // start at the start of the loop
         self.current_read_position = self.loop_start;
+
+        // set up fading position
+        self.fade_loop_end = self.loop_end + self.fade_length_samples;
+        self.fade_loop_start = self.loop_start + self.fade_length_samples;
+    }
+
+    pub fn set_fade_length(&mut self, length_samples: usize){
+        self.fade_length_samples = std::cmp::min(length_samples, self.loop_length());
+    }
+
+    fn loop_length(&self) -> usize {
+        return self.loop_end - self.loop_start + 1;
     }
 
     pub fn stop_looping(&mut self)
@@ -78,6 +104,7 @@ impl Looper
 
     fn tick_read_pos(&mut self) -> usize {
         self.current_read_position = wrap(self.current_read_position - 1, self.loop_end, self.loop_start);
+        // self.fading_read_position = wrap(self.fading_read_position - 1, self.fade_loop_end, self.fade_loop_start);
         return self.current_read_position;
     }
 }
@@ -112,6 +139,18 @@ mod tests {
     }
 
     #[test]
+    fn test_looper_readpos() {
+        let mut looper = Looper::new();
+        looper.set_looping_region(8, 4);
+        let mut out = vec![];
+        for _i in 0..10 {
+            out.push(looper.tick_read_pos());
+        }
+        let expected = vec![7, 6, 5, 4, 8, 7, 6, 5, 4, 8];
+        assert_eq!(out, expected);
+    }
+
+    #[test]
     fn test_looper_loop() {
         let mut looper = Looper::new();
 
@@ -131,15 +170,29 @@ mod tests {
         assert_eq!(out, expected);
     }
 
-    #[test]
-    fn test_looper_readpos() {
-        let mut looper = Looper::new();
-        looper.set_looping_region(8, 4);
-        let mut out = vec![];
-        for _i in 0..10 {
-            out.push(looper.tick_read_pos());
-        }
-        let expected = vec![7, 6, 5, 4, 8, 7, 6, 5, 4, 8];
-        assert_eq!(out, expected);
-    }
+
+    // TODO
+    // #[test]
+    // fn test_looper_fade() {
+    //     let mut looper = Looper::new();
+
+    //     // first half of buffer is 0
+    //     for _i in 0..4 {
+    //         looper.tick_delay(0.0);
+    //     }
+    //     // second half 1
+    //     for _i in 0..4 {
+    //         looper.tick_delay(1.0);
+    //     }
+
+    //     // set the loop region to whole buffer
+    //     looper.set_looping_region(8, 0);
+    //     looper.set_fade_length(4);
+
+    //     let mut out = vec![];
+    //     for _i in 0..10 {
+    //         out.push(looper.tick_loop());
+    //     }
+    //     let expected = vec!(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 0.0, 1.0, 2.0, 3.0);
+    // }
 }
