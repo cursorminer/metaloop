1ef51d7b8e670e9fd0fc13b8977fb6a80dff2995
rtu <rtu@ableton.com>
2024-06-01 19:26:11 +0200

    Turn into a nih-plug. Builds, and can be bundled if inside nih-plug repo

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-mod delay_line;
-mod grain;
-mod grain_looper;
-mod grain_player;
-mod ramped_value;
-mod stereo_pair;
+use nih_plug::prelude::*;
+
+use metaloop::Metaloop;
 
 fn main() {
-    println!("Hello, world!");
+    nih_export_standalone::<Metaloop>();
 }
