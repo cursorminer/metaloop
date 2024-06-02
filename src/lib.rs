f5584e600e6b9b682daea73347d71ac484aaecc7
rtu <rtu@ableton.com>
2024-06-02 12:26:05 +0200

    Tweak plugin metadata

--- src/lib.rs
+++ src/lib.rs
@@ -166,20 +166,20 @@ impl Plugin for Metaloop {
 }
 
 impl ClapPlugin for Metaloop {
-    const CLAP_ID: &'static str = "com.moist-plugins-gmbh-egui.gain-gui";
-    const CLAP_DESCRIPTION: Option<&'static str> = Some("A smoothed gain parameter example plugin");
+    const CLAP_ID: &'static str = "com.minersound.metaloop";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("A scrubber / looper");
     const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
     const CLAP_SUPPORT_URL: Option<&'static str> = None;
     const CLAP_FEATURES: &'static [ClapFeature] = &[
         ClapFeature::AudioEffect,
         ClapFeature::Stereo,
         ClapFeature::Mono,
-        ClapFeature::Utility,
+        ClapFeature::Glitch,
     ];
 }
 
 impl Vst3Plugin for Metaloop {
-    const VST3_CLASS_ID: [u8; 16] = *b"GainGuiYeahBoyyy";
+    const VST3_CLASS_ID: [u8; 16] = *b"MetaloopYeahBoyy";
     const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
         &[Vst3SubCategory::Fx, Vst3SubCategory::Tools];
 }
