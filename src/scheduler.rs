b4fe730759a6154b5f4bc709b851a4f6f0e5d3a6
rtu <rtu@ableton.com>
2024-06-04 20:57:25 +0200

    Scheduler schedules generic events

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,25 +1,20 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum LoopEvent {
-    StartGrain,
-    StopGrain,
-}
-
-pub struct Scheduler {
-    events: Vec<(f32, LoopEvent)>,
+// E is the event type
+pub struct Scheduler<E: Clone + Copy + PartialEq + Eq> {
+    events: Vec<(f32, E)>,
 }
 
 #[allow(dead_code)]
-impl Scheduler {
-    pub fn new() -> Scheduler {
+impl<E: Clone + Copy + PartialEq + Eq> Scheduler<E> {
+    pub fn new() -> Scheduler<E> {
         Scheduler { events: Vec::new() }
     }
 
-    pub fn schedule_event(&mut self, time: f32, event: LoopEvent) {
+    pub fn schedule_event(&mut self, time: f32, event: E) {
         assert!(time >= self.events.last().map(|&(t, _)| t).unwrap_or(0.0));
         self.events.push((time, event));
     }
 
-    pub fn tick(&mut self, time: f32) -> Vec<LoopEvent> {
+    pub fn tick(&mut self, time: f32) -> Vec<E> {
         let mut events = Vec::new();
         while let Some(&(event_time, ref event)) = self.events.first() {
             if event_time <= time {
@@ -41,22 +36,26 @@ impl Scheduler {
 mod tests {
     use super::*;
 
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+
+    pub enum TestEvent {
+        A,
+        B,
+    }
+
     #[test]
     fn test_scheduler() {
-        let mut scheduler = Scheduler::new();
-        scheduler.schedule_event(1.0, LoopEvent::StartGrain);
-        scheduler.schedule_event(2.0, LoopEvent::StopGrain);
-        scheduler.schedule_event(3.0, LoopEvent::StartGrain);
-        scheduler.schedule_event(4.0, LoopEvent::StopGrain);
-        scheduler.schedule_event(5.0, LoopEvent::StartGrain);
+        let mut scheduler = Scheduler::<TestEvent>::new();
+        scheduler.schedule_event(1.0, TestEvent::A);
+        scheduler.schedule_event(2.0, TestEvent::B);
+        scheduler.schedule_event(3.0, TestEvent::A);
+        scheduler.schedule_event(4.0, TestEvent::B);
+        scheduler.schedule_event(5.0, TestEvent::A);
         assert_eq!(scheduler.tick(0.0), vec![]);
-        assert_eq!(scheduler.tick(1.0), vec![LoopEvent::StartGrain]);
+        assert_eq!(scheduler.tick(1.0), vec![TestEvent::A]);
         assert_eq!(scheduler.tick(1.5), vec![]);
-        assert_eq!(scheduler.tick(2.0), vec![LoopEvent::StopGrain]);
-        assert_eq!(
-            scheduler.tick(4.0),
-            vec![LoopEvent::StartGrain, LoopEvent::StopGrain]
-        );
+        assert_eq!(scheduler.tick(2.0), vec![TestEvent::B]);
+        assert_eq!(scheduler.tick(4.0), vec![TestEvent::A, TestEvent::B]);
         assert_eq!(scheduler.tick(4.5), vec![]);
         scheduler.clear();
         assert_eq!(scheduler.tick(5.0), vec![]);
