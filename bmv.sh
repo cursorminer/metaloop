0cbbf358b92ecdf743bcc13ea22d5e2bd2d5c401
rtu <rtu@ableton.com>
2026-03-08 11:09:21 +0100

    Make build/move script --release

--- bmv.sh
+++ bmv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cargo xtask bundle metaloop;
+cargo xtask bundle metaloop --release;
 rm -r /Library/Audio/Plug-Ins/VST3/Metaloop.vst3;
 mv /Users/rtu/dev/Rust/projects/metaloop/target/bundled/Metaloop.vst3 /Library/Audio/Plug-Ins/VST3/
